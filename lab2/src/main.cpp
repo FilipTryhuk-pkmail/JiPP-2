45c6281fa2297f3de008d0046f907093e3ee93c2
FilipTryhuk <filip.tryhuk@student.pk.edu.pl>
2021-10-24 16:01:43 +0200

    all tasks finished

--- lab2/src/main.cpp
+++ lab2/src/main.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-//#include "lib2.h"
+#include <cmath>
 
 using namespace std;
 
@@ -74,6 +74,56 @@ void sort_arr(int tab[], int len){
     }
 }
 
+void change_int(int key, int *target){
+    *target = key;
+}
+
+void e_divide(int a, int b) {
+    int res;
+    float f_res;
+    try {
+        res = a / b;
+        f_res = (a * 1.) / b;
+        if (f_res - res > 0.0001) {throw "Niepoprawne dane!";}
+        else {cout << "Wynik dzielenia to: " << res << endl << endl;}
+    }
+    catch(const char* msg) {
+        cout << "Niepoprawne dane!" << endl;
+    }
+}
+
+void to_int(string s) {
+//    string letter;
+    int len = s.length();
+    int conv;
+/*
+    try {
+        for (int i = 0; i < len; i++) {
+            letter[0] = s[i];
+            if (stoi(letter) > 9 || stoi(letter) < 0) { throw "Wprowadzony ciąg zawiera nieprawidłowe znaki"; }
+        }
+    }
+    catch(const char* msg) {
+        cout << "Wprowadzony ciąg zawiera nieprawidłowe znaki." << endl;
+        return 0;
+    }
+*/
+    try {
+        conv = stoi(s);
+        if ((conv / pow(10, len-1)) < 1) {throw "Wprowadzony ciąg zawiera nieprawidłowe znaki.";}
+    }
+    catch(out_of_range) {
+        cout << "Liczba jest za duża." << endl;
+        return;
+    }
+    catch(const char* msg) {
+        cout << "Wprowadzony ciąg zawiera nieprawidłowe znaki." << endl;
+        return;
+    }
+
+    cout << "Podana liczba to: " << conv << endl << endl;
+}
+
 int main(int argc, char *argv[]) {
 
     //Generate three random numbers between 13 and 22
@@ -120,7 +170,30 @@ int main(int argc, char *argv[]) {
     sort_arr(tab, arr_size);
     if(arr_size > 4) {cout << "Sortowanie!" << endl << "Pierwsze pięć elementów tablicy: " << tab[0] << " " << tab[1] << " " << tab[2] << " " << tab[3] << " " << tab[4] << endl << endl;}
 
-    //TODO: ZADANIE 4 z wskaźników, obsługa błędów
+    //change x into w with a void func
+    int x = 10, w = 5;
+    int *w_ptr = &w;
+    cout << "W wynosi: " << w << " X wynosi:" << x << endl;
+    change_int(x, w_ptr);
+    cout << "Wpisanie X do W" << endl << "W wynosi: " << w << " X wynosi:" << x << endl;
+
+    //divide two numbers, catch exception if not possible
+    int a, b;
+    cout << endl << "Podaj pierwszą liczbę: ";
+    cin >> a;
+    cout << endl;
+    cout << "Podaj drugą liczbę: ";
+    cin >> b;
+    cout << endl;
+    e_divide(a, b);
+
+    //convert string of numbers to int
+    string st_inp;
+    cout << endl << "Podaj ciąg znaków: ";
+    cin >> st_inp;
+    cout << endl;
+    to_int(st_inp);
+
 
     return 0;
 }
